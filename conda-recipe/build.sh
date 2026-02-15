49005e5fc296c8aa1c928dacc196c6cb08fed2e1
Luohao Wang <luohaothu@live.com>
2026-02-15 17:09:21 +0800

    fix: add SPDX headers for conda recipe files
    Satisfy AUTOTEST check-license gate for conda packaging files.

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# Copyright (c) 1998 Lawrence Livermore National Security, LLC and other
+# HYPRE Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
 set -euxo pipefail
 
 HYPRE_ENABLE_MPI="${HYPRE_ENABLE_MPI:-OFF}"
